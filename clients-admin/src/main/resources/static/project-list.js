48a11930afb47de60bf5c09205c1bfebf51e37c8
cultivateweb <master4135719>
2017-08-26 12:35:41 +0300

    angular 1

---
+++ clients-admin/src/main/resources/static/project-list.js
@@ -0,0 +1,12 @@
+angular.module('project').value('projectListValue', [
+  {
+    name: 'AngularJS',
+    site: 'http://angularjs.org',
+    description: 'HTML enhanced for web apps!'
+  },
+  {
+    name: 'React',
+    site: 'https://facebook.github.io/react/',
+    description: 'A JavaScript library for building user interfaces.'
+  }
+])
