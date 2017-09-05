c11f2191ccf6421f9f3f9b7ed03a625d20fb5f99
DN121283MVP1 <vitalij.majstrenko@privatbank.ua>
2017-09-05 17:41:46 +0300

    Oltu client

--- clients-admin/src/main/java/com/github/cultivateweb/clients/controllers/LoginController.java
+++ clients-admin/src/main/java/com/github/cultivateweb/clients/controllers/LoginController.java
@@ -1,9 +1,30 @@
 package com.github.cultivateweb.clients.controllers;
 
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class LoginController {
+
+
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse response) throws OAuthSystemException, IOException {
+        OAuthClientRequest request=null;
+        request = OAuthClientRequest
+            .authorizationLocation("your server’s auth location ")
+            .setResponseType("code")
+            .setState("1")
+            .setClientId("your client id")
+            .setRedirectURI("<url>")
+            .buildQueryMessage();
+        System.out.println("Url for redirecting::::"+request.getLocationUri());
+        return new ModelAndView("redirect:"+request.getLocationUri());
+    }
 
 
     @RequestMapping( "/login")
